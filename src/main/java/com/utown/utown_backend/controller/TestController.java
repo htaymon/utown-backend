778ebfe582291eb2a930b414de3e0373d033531e
Htay Htay Mon <htayhtaymon@Htays-MacBook-Pro.local>
2026-03-31 15:46:15 +0900

    security configuration

---
+++ src/main/java/com/utown/utown_backend/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.utown.utown_backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Backend is running 🚀";
+    }
+}
